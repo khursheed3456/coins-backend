5c060769271ffb941f8abb35c90a2d4c23959bfe
Aap Ka Naam <asadullahmurad85@gmail.com>
2026-07-04 17:26:55 +0500

    fix

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -23,19 +23,12 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendOTPEmail(email, otp) {
 try {
-const response = await resend.emails.send({
-from: 'Investify [no-reply@investify45.qzz.io](mailto:no-reply@investify45.qzz.io)',
-to: email,
-subject: 'Your Investify Verification Code',
-text: `Your Investify OTP code is ${otp}. It will expire in 5 minutes. If you did not request this, please ignore this email.`,
-html: `         <div style="font-family: Arial, sans-serif; line-height: 1.6;">           <h2>Investify Verification</h2>           <p>Your OTP code is:</p>           <h1 style="letter-spacing: 2px;">${otp}</h1>           <p>This code will expire in <b>5 minutes</b>.</p>           <br/>           <p>If you did not request this, you can safely ignore this email.</p>           <hr/>           <p style="font-size: 12px; color: gray;">
-            © Investify - Secure Authentication System           </p>         </div>
-      `,
+await resend.emails.send({
+  from: 'no-reply@investify45.qzz.io',
+  to: email,
+  subject: 'oottpp',
+  text: `Your Investify ${otp}.`,
 });
-
-```
-console.log("Email sent:", response);
-```
 
 } catch (error) {
 console.error("Email error:", error);
